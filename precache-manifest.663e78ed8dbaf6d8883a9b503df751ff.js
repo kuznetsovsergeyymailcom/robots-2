873caffb95fed17fccb697db1e55c356b7054377
kuznetsovsergeyymailcom <kuznetsovsergey@ymail.com>
2020-02-23 18:08:58 +0300

    Updates

---
+++ precache-manifest.663e78ed8dbaf6d8883a9b503df751ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52faafc0e84e4c4b485c19dd6ce6622f",
+    "url": "/kuznetsovsergeyymailcom/robots-2/index.html"
+  },
+  {
+    "revision": "5b82f9d4aeff7b4bee3d",
+    "url": "/kuznetsovsergeyymailcom/robots-2/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "725338a13411881e4d4f",
+    "url": "/kuznetsovsergeyymailcom/robots-2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5b82f9d4aeff7b4bee3d",
+    "url": "/kuznetsovsergeyymailcom/robots-2/static/js/2.2be83f45.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kuznetsovsergeyymailcom/robots-2/static/js/2.2be83f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725338a13411881e4d4f",
+    "url": "/kuznetsovsergeyymailcom/robots-2/static/js/main.41dba4ef.chunk.js"
+  },
+  {
+    "revision": "73e1f5b246588713957c",
+    "url": "/kuznetsovsergeyymailcom/robots-2/static/js/runtime-main.9c791dbe.js"
+  }
+]);
